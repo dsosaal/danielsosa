db869a4a95eca06819b6fdd527c031ca3aabe4cb
dsosaal <sosaalamillodaniel@gmail.com>
2025-04-03 20:42:52 -0700

    funcionalidades agregadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,3 +180,30 @@ sueContactForm.addEventListener('submit', function(event) {
             sueContactFormAlert.title = error;
         });
 });
+
+
+// Shrink the height of the header on scroll
+window.addEventListener("scroll", () => {
+    const sueHeader = document.querySelector(".sue-header");
+
+    sueHeader.classList.toggle("shrink", window.scrollY > 0);
+});
+
+// Bottom Navigation Menu
+// Each Botton navigation menu items active on page scroll.
+window.addEventListener("scroll", () =>{
+    const navMenuSections = document.querySelectorAll(".nav-menu-section");
+    const scrollY = window.pageYOffset;
+
+    navMenuSections.forEach((navMenuSection) => {
+        let sectionHeight = navMenuSection.offsetHeight;
+        let sectionTop = navMenuSection.offsetTop - 50;
+        let id = navMenuSection.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.add("current");
+        }else{
+            document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.remove("current");
+        }
+    });
+});
